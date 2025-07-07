5e791b13c9e6fbe17545dd882ddb660fcca485c0
Robbie <robbie331@>
2025-07-07 15:34:41 +0900

    Level 1 - 4 사용자가 exit 문자열을 입력하기 전까지 반복 { 정수 2개와 사칙연산 기호 입력 받음 기호에 따라 연산 후 결과 출력 }

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,61 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //exit 입력 전까지 반복
+        while(true){
+            //정수 2개 입력 받음
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            //사칙연산 기호 입력 받아 char 형태로 저장
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String symbol = sc.next();
+            char operator = symbol.charAt(0);
+
+            int result = 0; /*결과 저장*/
+            boolean error = false; /*부적절한 입력 확인*/
+            switch (operator){
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if(num2 == 0){
+                        System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다. ");
+                        error = true;
+                    } else{
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    System.out.println("사칙연산 기호가 잘못 입력되었습니다.");
+                    error = true;
+            }
+            if(!error) System.out.println("연산 결과: " + result);
+
+            //사용자가 exit 입력 시 반복 종료
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String isExit = sc.next();
+            if(isExit.equals("exit")) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
+        }
+
+
 
     }
 }
