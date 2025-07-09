dde1bee3c36519ed99581a1072fd55148f01e444
Robbie <robbie331@>
2025-07-09 17:07:23 +0900

    Level 3 - 1 Enum 타입으로 연산자에 대한 정보를 관리하고 이를 ArithmeticCalculator 클래스에 활용

---
+++ src/main/java/level3/ArithmeticCalculator.java
@@ -0,0 +1,97 @@
+package level3;
+import java.util.List;
+
+enum OperatorType{
+    ADD('+'){
+        public double operate(double num1, double num2){
+            return num1 + num2;
+        }
+    },
+    SUBTRACK('-'){
+        public double operate(double num1, double num2){
+            return num1 - num2;
+        }
+    },
+    MULTIPLY('*'){
+        public double operate(double num1, double num2){
+            return num1 * num2;
+        }
+    },
+    DIVIDE('/'){
+        public double operate(double num1, double num2){
+            if(num2 == 0){
+                throw new ArithmeticException("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+            } else{
+                return num1 / num2;
+            }
+        }
+    },
+    MOD('%'){
+        public double operate(double num1, double num2){
+            if(num2 == 0){
+                throw new ArithmeticException("나머지 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+            } else{
+                return num1 % num2;
+            }
+        }
+    };
+
+    private final char symbol;        /*연산 기호 저장 필드*/
+
+    OperatorType(char symbol){  /*생성자*/
+        this.symbol = symbol;
+    }
+
+    /*현재 객체가 가진 symbol과 입력 받은 연산 기호가 같은지 판단*/
+    public boolean isAvailable(char symbol){
+        return symbol == this.symbol;
+    }
+
+    /*enum 클래스에서 공통으로 사용할 메서드는 추상 메서드로 정의 필요*/
+    public abstract double operate(double num1, double num2);
+}
+
+class ArithmeticCalculator extends Calculator {
+
+    /*사칙연산의 각 기능을 담당하는 enum 타입들을 저장하는 List*/
+    private final List<OperatorType> operators;
+
+    /*인스턴스 생성 시 필드(List) 초기화, enum 클래스가 가지고 있는 모든 상수 값을 List에 저장*/
+    public ArithmeticCalculator(){
+        this.operators = List.of(OperatorType.values());
+    }
+
+    public List<Double> get(){
+        return results;
+    }
+
+    public void set(List<Double> results){
+        this.results = results;
+    }
+
+    /*사용자가 입력한 연산자(char)에 해당하는 연산 수행 후 반환*/
+    /*분모가 0이거나 연산 기호가 잘못된 경우, 의도적으로 예외를 발생시킨 다음 상위 메서드에 전달*/
+    public double calculate(double num1, double num2, char operator) {
+        double result;
+        for(OperatorType o : operators){                /*List 내 모든 객체 확인*/
+            if(o.isAvailable(operator)){            /*해당 객체가 할 수 있는 연산인지 판단*/
+                result = o.operate(num1, num2);
+                results.add(result);
+                return result;
+            }
+        }
+        throw new ArithmeticException("사칙연산 기호가 잘못 입력되었습니다.");  /*가능한 연산이 아닌 경우*/
+    }
+
+    /*가장 먼저 저장된 연산 결과 삭제*/
+    public void removeResult(){
+        if(!results.isEmpty()){
+            results.remove(0);
+        }
+    }
+
+    /*현재 저장되어 있는 연산 결과 모두 출력*/
+    public void inquiryResults(){
+        System.out.println(results);
+    }
+}
---
+++ src/main/java/level3/Calculator.java
@@ -0,0 +1,26 @@
+package level3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Calculator {
+    /* static: 클래스 단위로 존재하며 모든 인스턴스가 공유
+       final: 한 번 초기화되면 변경 불가능
+       PI는 모든 인스턴스가 고정된 값을 사용하고 변경될 수 없으므로 static final (상수)로 선언함 */
+    public static final double PI = 3.141592;
+
+    public List<Double> results;
+
+    /*생성 시 필드가 초기화되도록 생성자 구현*/
+    public Calculator(){
+        results = new ArrayList<>();
+    }
+
+    /*가장 먼저 저장된 데이터 삭제*/
+    abstract void removeResult();
+
+    /*현재 저장되어 있는 데이터 모두 출력*/
+    abstract void inquiryResults();
+
+
+}
